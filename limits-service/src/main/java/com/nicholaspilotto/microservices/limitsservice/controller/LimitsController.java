d0a1b9502231a68eb85c18a173df18c16e647533
Nicholas Pilotto <nicholaspilotto@gmail.com>
2023-04-11 18:54:13 +0200

    Created limits classes

--- limits-service/src/main/java/com/nicholaspilotto/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/nicholaspilotto/microservices/limitsservice/controller/LimitsController.java
@@ -1,2 +1,13 @@
-package com.nicholaspilotto.microservices.limitsservice.controller;public class LimitsController {
+package com.nicholaspilotto.microservices.limitsservice.controller;
+
+import com.nicholaspilotto.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+  @GetMapping("/limits")
+  public Limits retrieveLimits() {
+    return new Limits(1, 100);
+  }
 }
